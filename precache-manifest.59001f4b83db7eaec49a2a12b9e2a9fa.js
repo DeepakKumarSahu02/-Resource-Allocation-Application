4c23bb332be7dd227635665257cc5aa8ecc86b52
Deepak Kumar Sahu <deepaksahu289@gmail.com>
2019-10-02 10:06:38 +0530

    Updates

---
+++ precache-manifest.59001f4b83db7eaec49a2a12b9e2a9fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642c616c20c2812102b473d1c2f6c65f",
+    "url": "/-Resource-Allocation-Application/index.html"
+  },
+  {
+    "revision": "97d65dfb22564bbff780",
+    "url": "/-Resource-Allocation-Application/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a335505d97614982ed76",
+    "url": "/-Resource-Allocation-Application/static/css/main.ada48f17.chunk.css"
+  },
+  {
+    "revision": "97d65dfb22564bbff780",
+    "url": "/-Resource-Allocation-Application/static/js/2.efbdf47e.chunk.js"
+  },
+  {
+    "revision": "a335505d97614982ed76",
+    "url": "/-Resource-Allocation-Application/static/js/main.9a9d680d.chunk.js"
+  },
+  {
+    "revision": "63d70a56a87bbf0e2811",
+    "url": "/-Resource-Allocation-Application/static/js/runtime-main.55e4b276.js"
+  }
+]);
